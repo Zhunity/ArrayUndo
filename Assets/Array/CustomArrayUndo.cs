719551fcc5ec26f04b23c6b643221ff27693a452
lihua <poinseting@163.com>
2020-04-14 00:44:45 +0800

    自定义数组

---
+++ Assets/Array/CustomArrayUndo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CustomArray<T>
+{
+	public T[] Array;
+	public T this[int index]
+	{
+		get
+		{
+			return Array[index];
+		}
+	}
+	public CustomArray()
+	{
+		this.Array = new T[4];
+	}
+
+	public CustomArray(int length)
+	{
+		this.Array = new T[length];
+	}
+}
+
+public class CustomArrayUndo : MonoBehaviour
+{
+}
