f8c2cbc4e6dc522126924fbad9e13b3fe438c1a0
lihua <poinseting@163.com>
2020-04-13 23:02:40 +0800

    添加Window父类

--- Assets/Array/IntArrayUndo.cs
+++ Assets/Array/IntArrayUndo.cs
@@ -7,10 +7,8 @@ public class IntArrayUndo : ArrayUndo<int>
 {
 }
 
-public class IntArrayUndoWindow : EditorWindow
+public class IntArrayUndoWindow : UndoWindow<int>
 {
-	public IntArrayUndo array = null;
-	public int value = 1;
 
 	[MenuItem("Undo/Int")]
 	static void OpenWindow()
@@ -19,59 +17,17 @@ public class IntArrayUndoWindow : EditorWindow
 		window.Show();
 	}
 
-	private void OnEnable()
+	protected override void ChangeValue()
 	{
-		Undo.undoRedoPerformed += this.UndoRedoCallback;
-	}
-
-	public void OnGUI()
-	{
-		GUILayout.Space(5);
-
-		array = (IntArrayUndo)EditorGUILayout.ObjectField(array, typeof(IntArrayUndo), true);
-
-		GUILayout.Label("Current value");
-
-		GUILayout.Label("value: " + value.ToString());
-
-		if (GUILayout.Button("Expand", GUILayout.Height(17)))
+		if (GUILayout.Button("Add", GUILayout.Height(17)))
 		{
-			value *= 2;
+			value += 2;
 		}
 
-		if (GUILayout.Button("Shrink", GUILayout.Height(17)))
+		if (GUILayout.Button("Sub", GUILayout.Height(17)))
 		{
-			value /= 2;
+			value -= 2;
 		}
-
-		if (GUILayout.Button("Apply", GUILayout.Height(20)))
-		{
-			ApplyVertexFactors(value);
-		}
-	}
-
-	public void ApplyVertexFactors(int factor)
-	{
-		if (array == null)
-			return;
-
-
-		Undo.RecordObject(array, "Modify Vertices");
-
-		array.Add(factor);
-		array.Log();
-	}
-
-	private void UndoRedoCallback()
-	{
-		if(array == null)
-		{
-			return;
-		}
-
-		int[] copy = array.array;
-		array.array = copy;
-		array.index = array.index;
 	}
 }
 
---
+++ Assets/GenericParent/UndoWindow.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GenericWindow<T> : EditorWindow where T : MonoBehaviour
+{
+}
+
+
+public class UndoWindow<T> : GenericWindow<ArrayUndo<T>>
+{
+	public ArrayUndo<T> array = null;
+	public T value;
+
+	private void OnEnable()
+	{
+		Undo.undoRedoPerformed += this.UndoRedoCallback;
+		array = GameObject.FindObjectOfType<ArrayUndo<T>>();
+	}
+
+	public void OnGUI()
+	{
+		GUILayout.Space(5);
+
+		array = (ArrayUndo<T>)EditorGUILayout.ObjectField(array, typeof(ArrayUndo<T>), true);
+
+		GUILayout.Label("Current value");
+		GUILayout.Label("value: " + value.ToString());
+
+		ChangeValue();
+
+		if (GUILayout.Button("Apply", GUILayout.Height(20)))
+		{
+			ApplyVertexFactors(value);
+		}
+	}
+
+	protected virtual void ChangeValue()
+	{
+	}
+
+	public void ApplyVertexFactors(T factor)
+	{
+		if (array == null)
+			return;
+
+
+		Undo.RecordObject(array, "Modify Vertices");
+
+		array.Add(factor);
+		array.Log();
+	}
+
+	private void UndoRedoCallback()
+	{
+		if (array == null)
+		{
+			return;
+		}
+
+		T[] copy = array.array;
+		array.array = copy;
+		array.index = array.index;
+	}
+}
